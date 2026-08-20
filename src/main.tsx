030c98f73940f1529c2064ba96377249dc589c22
ushyndali <uldana.shyndali@prowheelxr.com>
2026-08-20 12:48:39 +0500

    Fix hash route base

--- src/main.tsx
+++ src/main.tsx
@@ -7,10 +7,11 @@ import './index.css';
 import { isGitHubPagesBuild, restoreGitHubPagesRoute, routerBase } from './lib/routes';
 
 restoreGitHubPagesRoute();
+const routeBase = isGitHubPagesBuild ? '' : routerBase;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router base={routerBase} hook={isGitHubPagesBuild ? useHashLocation : undefined}>
+    <Router base={routeBase} hook={isGitHubPagesBuild ? useHashLocation : undefined}>
       <App />
     </Router>
   </React.StrictMode>,
